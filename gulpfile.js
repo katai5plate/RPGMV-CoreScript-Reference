c0039834737afceb2c2cc20ebace9e758b918330
katai5plate <katai5plate.sub@gmail.com>
2018-11-05 04:09:19 +0900

    add replace function

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var htmlbeautify = require("gulp-html-beautify");
 var markdown = require("gulp-breakdance");
 var rename = require("gulp-rename");
+var replace = require("gulp-replace");
 
 gulp.task("normal", () => {
   gulp
@@ -20,5 +21,12 @@ gulp.task("convert", () => {
     .pipe(rename({
       extname: ".md"
     }))
+    .pipe(gulp.dest("./docs/jsdoc/"));
+});
+
+gulp.task("replace", () => {
+  gulp
+    .src("./docs/jsdoc/**/*.html")
+    .pipe(replace(/\[(.*?)\]\((.*?)\.html\)/g,"[$1]($2.md)"))
     .pipe(gulp.dest("./docs/jsdoc/"));
 });
